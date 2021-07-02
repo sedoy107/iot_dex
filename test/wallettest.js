d0349506b0f118bc7905a0bd8d9eeb7b8044619a
sgorbov <noname@FC008866.local>
2021-07-02 08:27:42 -0500

    Create wallettest.js

---
+++ test/wallettest.js
@@ -0,0 +1,7 @@
+const Wallet = artifacts.require("Wallet");
+
+const truffleAssert = require('../truffle-assertions');
+
+// references for convenience
+const toBN = web3.utils.toBN;
+const fromUtf8 = web3.utils.fromUtf8;
