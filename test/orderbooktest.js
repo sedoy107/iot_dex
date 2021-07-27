f1e4cf0542e77fe7dd2faec44314e031d2ef9f91
sgorbov <noname@FC008866.local>
2021-07-27 05:22:06 -0500

    Rewrite orderbook tests. Correct testing of market orders

--- test/orderbooktest.js
+++ test/orderbooktest.js
@@ -1,146 +1,158 @@
-'use strict';
+'use strict'
 
-const OrderBook = artifacts.require("OrderBook");
+const OrderBook = artifacts.require("OrderBook")
 
-const truffleAssert = require('../node_modules/truffle-assertions');
-const Chance = require('../node_modules/chance');
-const chance = new Chance();
+const truffleAssert = require('../node_modules/truffle-assertions')
+const Chance = require('../node_modules/chance')
+const truffleAssertions = require('../node_modules/truffle-assertions')
+const chance = new Chance()
 
 // references for convenience
-const toBN = web3.utils.toBN;
-const fromUtf8 = web3.utils.fromUtf8;
+const toBN = web3.utils.toBN
+const fromUtf8 = web3.utils.fromUtf8
 
 // Order book side constants
-const BUY = 0;
-const SELL = 1;
+const BUY = 0
+const SELL = 1
 
 // Order types constants
-const MARKET = 0;
-const LIMIT = 1;
+const MARKET = 0
+const LIMIT = 1
+
+// Define ticker values
+const tickerTo = web3.utils.fromAscii("LINK")
+const tickerFrom = web3.utils.fromAscii("MATIC")
  
-contract("OrderBook Test", async accounts => {
+contract("OrderBook: Error Conditions", async accounts => {
 
     let orderBook
 
-    const tickerTo = web3.utils.fromAscii("LINK");
-    const tickerFrom = web3.utils.fromAscii("MATIC");
-
     before("Setup OrderBook contract", async () => {
-        orderBook = await OrderBook.deployed();
+        orderBook = await OrderBook.deployed()
     })
 
-    describe("Error conditions", async () => {
-        it("should revert when try to place order with amount == 0", async () => {
-            await truffleAssert.reverts(
-                orderBook.createOrder(BUY, LIMIT, tickerTo, tickerFrom, 0, 0)
-            )
-            await truffleAssert.reverts(
-                orderBook.createOrder(SELL, LIMIT, tickerTo, tickerFrom, 0, 0)
-            )
-        });
-        it("should not place market orders when order book is empty", async () => {
-            await truffleAssert.reverts(
-                orderBook.createOrder(BUY, MARKET, tickerTo, tickerFrom, 0, 5)
-            )
-            await truffleAssert.reverts(
-                orderBook.createOrder(SELL, MARKET, tickerTo, tickerFrom, 0, 5)
-            )
-        });
-        it("should revert when try to get market price when market is not available", async () => {
-            await truffleAssert.reverts(
-                orderBook.getMarketPrice(BUY, tickerTo, tickerFrom)
-            )
-            await truffleAssert.reverts(
-                orderBook.getMarketPrice(SELL, tickerTo, tickerFrom)
-            )
-        });
-    });
-    describe("Limit Orders", async () => {
-        it("should place BUY orders sorted from lowest [0] to highest [length - 1]", async () => {
-            /**
-             * THE BUY order with the lowest price must be at the top of the SELL order book
-             *
-             * | Price | Amount |
-             * |   2   |    4   | [0]
-             * |   5   |    8   | [1]
-             * |   8   |   12   | [2]
-             * |  10   |   13   | [3]
-            */
+    it("should revert when try to place order with amount == 0", async () => {
+        await truffleAssert.reverts(
+            orderBook.createOrder(BUY, LIMIT, tickerTo, tickerFrom, 0, 0)
+        )
+        await truffleAssert.reverts(
+            orderBook.createOrder(SELL, LIMIT, tickerTo, tickerFrom, 0, 0)
+        )
+    })
+    it("should not place market orders when order book is empty", async () => {
+        await truffleAssert.reverts(
+            orderBook.createOrder(BUY, MARKET, tickerTo, tickerFrom, 0, 5)
+        )
+        await truffleAssert.reverts(
+            orderBook.createOrder(SELL, MARKET, tickerTo, tickerFrom, 0, 5)
+        )
+    })
+    it("should revert when try to get market price when market is not available", async () => {
+        await truffleAssert.reverts(
+            orderBook.getMarketPrice(BUY, tickerTo, tickerFrom)
+        )
+        await truffleAssert.reverts(
+            orderBook.getMarketPrice(SELL, tickerTo, tickerFrom)
+        )
+    })
+})
 
-            let orderCount = 20;
+contract("OrderBook: Market Orders", async accounts => {
 
-            for (let i = 0; i < orderCount; i++) {
-                await orderBook.createOrder(BUY, LIMIT, tickerTo, tickerFrom, chance.integer({min:1, max:100}), 4);
-            }
+    let orderBook
 
-            let orders = await orderBook.getOrderBook(BUY, tickerTo, tickerFrom);
-            let prices = orders.map(x => parseInt(x.price));
-            let length = prices.length;
+    before("Fill order book with limit orders that cannot be matched", async () => {
 
-            assert.equal(Math.max.apply(null,prices), prices[length - 1], "Wrong maximum");
-            assert.equal(Math.min.apply(null,prices),prices[0], "Wrong minimum");
-            for (let i = 1; i < orderCount; i++) {
-                assert(prices[i - 1] <= prices[i], "Wrong order")
-            }
-        });
-        it("should place SELL orders sorted from highest [0] to lowest [length - 1]", async () => {
-            /**
-             * The SELL order with the highest price must be at the top of the BUY order book
-             *
-             * | Price | Amount |
-             * |   2   |   10   | [3]
-             * |   5   |    8   | [2]
-             * |   8   |    6   | [1]
-             * |  10   |    5   | [0]
-             */
+        orderBook = await OrderBook.deployed()
 
-            let orderCount = 5;
+        await orderBook.createOrder(SELL, LIMIT, tickerTo, tickerFrom, 10, 5) // sell 5@10
+        await orderBook.createOrder(SELL, LIMIT, tickerTo, tickerFrom, 9, 6) // sell 6@9 <- topSellOrder
+        await orderBook.createOrder(SELL, LIMIT, tickerTo, tickerFrom, 11, 4) // sell 4@11
+        await orderBook.createOrder(BUY, LIMIT, tickerTo, tickerFrom, 7, 6) // buy 6@7 <- topBuyOrder
+        await orderBook.createOrder(BUY, LIMIT, tickerTo, tickerFrom, 6, 8) // buy 8@6
+        await orderBook.createOrder(BUY, LIMIT, tickerTo, tickerFrom, 5, 10) // buy 10@5
+    })
 
-            for (let i = 0; i < orderCount; i++) {
-                await orderBook.createOrder(SELL, LIMIT, tickerTo, tickerFrom, chance.integer({min:1, max:100}), 4);
-            }
+    it("should place SELL orders at the top of the order book [length - 1]", async () => {
+        await orderBook.createOrder(SELL, MARKET, tickerTo, tickerFrom, 0, 20) // sell 20@market
 
-            let orders = await orderBook.getOrderBook(SELL, tickerTo, tickerFrom);
-            let prices = orders.map(x => parseInt(x.price));
-            let length = prices.length;
+        let topSellOrder = (await orderBook.getOrderBook(SELL, tickerTo, tickerFrom)).slice(-1)[0].price
+        let topBuyOrder = (await orderBook.getOrderBook(BUY, tickerTo, tickerFrom)).slice(-1)[0].price
+        
+        assert.equal(topSellOrder, topBuyOrder, "Market SELL order price must match the top BUY order price")
 
-            assert.equal(Math.max.apply(null,prices), prices[0], "Wrong maximum");
-            assert.equal(Math.min.apply(null,prices),prices[length - 1], "Wrong minimum");
-            for (let i = 1; i < orderCount; i++) {
-                assert(prices[i - 1] >= prices[i], "Wrong order")
-            }
-        });
-    });
-    describe("Market Orders", async () => {
-        it("should place SELL orders at the top of the order book [length - 1]", async () => {
+    })
+    it("should place BUY orders at the top of the order book [length - 1]", async () => {
+        await orderBook.createOrder(BUY, MARKET, tickerTo, tickerFrom, 0, 30) // buy 30@market
 
-            // Retrieve bestPrice
-            let orders = await orderBook.getOrderBook(SELL, tickerTo, tickerFrom);
-            let prices = orders.map(x => parseInt(x.price));
-            let bestPrice = prices.slice(-1)[0];
+        let topSellOrder = (await orderBook.getOrderBook(SELL, tickerTo, tickerFrom)).slice(-1)[0].price
+        let topBuyOrder = (await orderBook.getOrderBook(BUY, tickerTo, tickerFrom)).slice(-1)[0].price
+        
+        assert.equal(topBuyOrder, topSellOrder, "Market BUY order price must match the top SELL order price")
+    })
+})
 
-            await orderBook.createOrder(SELL, MARKET, tickerTo, tickerFrom, 0, 4);
+contract("OrderBook: Limit Orders", async accounts => {
 
-            orders = await orderBook.getOrderBook(SELL, tickerTo, tickerFrom);
-            prices = orders.map(x => parseInt(x.price));
+    let orderBook
 
-            assert.equal(Math.min.apply(null,prices), bestPrice, "bestPrice must be minimum");
-            assert.equal(prices.slice(-1)[0], prices.slice(-2)[0], "The two top SELL orders must have the same price")
-        });
-        it("should place BUY orders at the top of the order book [length - 1]", async () => {
-            
-            // Retrieve bestPrice
-            let orders = await orderBook.getOrderBook(BUY, tickerTo, tickerFrom);
-            let prices = orders.map(x => parseInt(x.price));
-            let bestPrice = prices.slice(-1)[0];
+    before("Setup OrderBook contract", async () => {
+        orderBook = await OrderBook.deployed()
+    })
+    it("should place BUY orders sorted from lowest [0] to highest [length - 1]", async () => {
+        /**
+         * THE BUY order with the lowest price must be at the top of the SELL order book
+         *
+         * | Price | Amount |
+         * |   2   |    4   | [0]
+         * |   5   |    8   | [1]
+         * |   8   |   12   | [2]
+         * |  10   |   13   | [3]
+        */
 
-            await orderBook.createOrder(BUY, MARKET, tickerTo, tickerFrom, 0, 4);
+        let orderCount = 20
 
-            orders = await orderBook.getOrderBook(BUY, tickerTo, tickerFrom);
-            prices = orders.map(x => parseInt(x.price));
+        for (let i = 0; i < orderCount; i++) {
+            await orderBook.createOrder(BUY, LIMIT, tickerTo, tickerFrom, chance.integer({min:1, max:100}), 4)
+        }
 
-            assert.equal(Math.max.apply(null,prices), bestPrice, "bestPrice must be minimum");
-            assert.equal(prices.slice(-1)[0], prices.slice(-2)[0], "The two top SELL orders must have the same price")
-        });
-    });
-});
+        let orders = await orderBook.getOrderBook(BUY, tickerTo, tickerFrom)
+        let prices = orders.map(x => parseInt(x.price))
+        let length = prices.length
+
+        assert.equal()
+
+        assert.equal(Math.max.apply(null,prices), prices[length - 1], "Wrong maximum")
+        assert.equal(Math.min.apply(null,prices),prices[0], "Wrong minimum")
+        for (let i = 1; i < orderCount; i++) {
+            assert(prices[i - 1] <= prices[i], "Wrong order")
+        }
+    })
+    it("should place SELL orders sorted from highest [0] to lowest [length - 1]", async () => {
+        /**
+         * The SELL order with the highest price must be at the top of the BUY order book
+         *
+         * | Price | Amount |
+         * |   2   |   10   | [3]
+         * |   5   |    8   | [2]
+         * |   8   |    6   | [1]
+         * |  10   |    5   | [0]
+         */
+
+        let orderCount = 5;
+
+        for (let i = 0; i < orderCount; i++) {
+            await orderBook.createOrder(SELL, LIMIT, tickerTo, tickerFrom, chance.integer({min:1, max:100}), 4)
+        }
+
+        let orders = await orderBook.getOrderBook(SELL, tickerTo, tickerFrom)
+        let prices = orders.map(x => parseInt(x.price))
+        let length = prices.length
+
+        assert.equal(Math.max.apply(null,prices), prices[0], "Wrong maximum")
+        assert.equal(Math.min.apply(null,prices),prices[length - 1], "Wrong minimum")
+        for (let i = 1; i < orderCount; i++) {
+            assert(prices[i - 1] >= prices[i], "Wrong order")
+        }
+    })
+})
