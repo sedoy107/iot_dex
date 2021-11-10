3ae67e3817c24483cb2661c2b6d825d73349c3ee
sgorbov <noname@FC008866.local>
2021-11-10 04:34:57 -0600

    mint more tokens

--- contracts/Tokens.sol
+++ contracts/Tokens.sol
@@ -5,13 +5,13 @@ pragma solidity ^0.8.0;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Link is ERC20 ("Chainlink", "LINK") {
-    constructor () { _mint(msg.sender, 1000); }
+    constructor () { _mint(msg.sender, 1000000000000000000000); }
 }
 
 contract Polygon is ERC20 ("Polygon", "MATIC") {
-    constructor () { _mint(msg.sender, 1000); }
+    constructor () { _mint(msg.sender, 1000000000000000000000); }
 }
 
 contract WrappedBitcoin is ERC20 ("Wrapped Bitcoin", "WBTC") {
-    constructor () { _mint(msg.sender, 1000); }
+    constructor () { _mint(msg.sender, 1000000000000000000000); }
 }
