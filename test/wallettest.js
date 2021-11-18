8347fe0fb02748f2c71ec8931db4c7aa50055969
sgorbov <noname@FC008866.local>
2021-11-18 06:07:22 -0600

    Wallet tests updated

--- test/wallettest.js
+++ test/wallettest.js
@@ -9,20 +9,28 @@ const truffleAssert = require('../node_modules/truffle-assertions');
 const toBN = web3.utils.toBN;
 const fromUtf8 = web3.utils.fromUtf8;
  
+let wallet
+let link
+let symbol
+let ticker
+
+const ethTicker = fromUtf8("ETH")
+const ethAddress = '0x0000000000000000000000000000000000000000'
+const ethDecimals = 18
+
 contract("Wallet Test", async accounts => {
 
-    let wallet
-    let link
-    let symbol
-    let ticker
-    
-    const ethTicker = fromUtf8("ETH");
+    let balances = [
+        {accountId: 1, quantity: toBN(5 * (10 ** 18))},
+        {accountId: 2, quantity: toBN(4 * (10 ** 18))},
+        {accountId: 3, quantity: toBN(3 * (10 ** 18))},
+    ]
 
-    before("Setup contracts", async () => {
-        wallet = await Wallet.deployed();
-        link = await Link.deployed();
-        symbol = await link.symbol();
-        ticker = fromUtf8(symbol);
+    before("Setup contracts ", async () => {
+        wallet = await Wallet.deployed()
+        link = await Link.deployed()
+        symbol = await link.symbol()
+        ticker = fromUtf8(symbol)
     })
 
     describe("Add ERC20 to wallet", async () => {
@@ -34,77 +42,93 @@ contract("Wallet Test", async accounts => {
                 wallet.addToken(ticker, link.address, {from: accounts[0]})
             );
         });
+        it("should not be possible to add same token more than once", async () => {
+            await truffleAssert.reverts (
+                wallet.addToken(ticker, link.address, {from: accounts[0]})
+            )
+        });
+        it("should not be possible to add ETH token as it is being added in the constructor", async () => {
+            await truffleAssert.reverts (
+                wallet.addToken(ethTicker, link.address, {from: accounts[0]})
+            )
+        });
     });
     describe("ETHER deposit / withdrawal", async () => {
+
+        it("should not allow withdrawals when an account has 0 balance", async () => {
+            await truffleAssert.reverts( wallet.methods['withdraw(uint256)'](100, {from: accounts[4]}) );
+        })
         it("should handle deposits correctly", async () => {
-            let balances = {
-                1 : 10 ** 5,
-                2 : 10 ** 4,
-                3 : 10 ** 3,
-            }
+            for (let i = 0; i < balances.length; i++) {
+                const {accountId, quantity} = balances[i]
+                await wallet.methods['deposit()']({ from: accounts[accountId], value: quantity });
 
-            for ( let i in balances ) {
-                await wallet.methods['deposit()']({from: accounts[i], value: balances[i]});
+                let accountBalance = await wallet.balances(accounts[accountId], ethTicker);
 
-                let accountBalance = (await wallet.balances(accounts[i], ethTicker)).toNumber();
-
-                console.log("deposited to accounts[" + i + "]:" + accountBalance);
-
-                assert.equal(accountBalance, balances[i], "Balances do not match");
+                console.log("deposited to accounts[" + accountId + "]:" + accountBalance);
+                
+                assert(accountBalance.eq(quantity), "Balances do not match");
             }
         });
-        it("should handle withdrawals correctly", async () => {
-            await truffleAssert.reverts ( wallet.withdraw(100, {from: accounts[4]}) );
-
-            let balances = {
-                1 : 10 ** 5,
-                2 : 10 ** 4,
-                3 : 10 ** 3,
+        it("should not allow withdrawals when an account insufficient balance", async () => {
+            for (let i = 0; i < balances.length; i++) {
+                const {accountId, quantity} = balances[i]
+                await truffleAssert.reverts(wallet.methods['withdraw(uint256)'](quantity+1, { from: accounts[accountId] }))
             }
+        })
+        it("should handle withdrawals correctly", async () => {
+            for (let i = 0; i < balances.length; i++) {
+                const {accountId, quantity} = balances[i]
+                await truffleAssert.passes(wallet.methods['withdraw(uint256)'](quantity, { from: accounts[accountId] }));
 
-            for ( let i in balances ) {
-                await truffleAssert.reverts ( wallet.methods['withdraw(uint256)'](balances[i] + 1, {from: accounts[i]}) );
-                await truffleAssert.passes ( wallet.methods['withdraw(uint256)'](balances[i], {from: accounts[i]}) );
+                let accountBalance = await wallet.balances(accounts[accountId], ethTicker);
 
-                let accountBalance = (await wallet.balances(accounts[i], ethTicker)).toNumber();
+                console.log("withdrawn from accounts[" + accountId + "]:" + quantity);
 
-                console.log("withdrawn from accounts[" + i + "]:" + balances[i]);
-                
-                assert.equal(accountBalance, 0, "Balance MUST be zero");
+                assert(accountBalance.eq(toBN(0)), "Balance MUST be zero");
             }
         });
     });
     describe("ERC20 deposit / withdrawal", async () => {
+        before("Send ERC20 tokens to participating accounts", async () => {
+            for (let i = 0; i < balances.length; i++) {
+                const {accountId, quantity} = balances[i]
+                await link.transfer(accounts[accountId], quantity)
+                await link.approve(await wallet.address, quantity, {from: accounts[accountId]})
+            }
+        })
+        it("should not allow withdrawals when an account has 0 balance", async () => {
+            await truffleAssert.reverts( wallet.methods['withdraw(uint256,bytes32)'](100, ticker, {from: accounts[4]}) );
+        })
         it("should handle deposits correctly", async () => {
-            await wallet.addToken(ticker, link.address, {from: accounts[0]})
-            await link.approve(await wallet.address, 500);
-            await wallet.deposit(100, ticker);
-            let balanceOfLink = await wallet.balances(accounts[0], ticker);
-            assert.equal(
-                balanceOfLink.toNumber(), 100
-            );
-            let erc20Balance = await link.balanceOf(accounts[0]);
-            assert.equal(
-                erc20Balance.toNumber(), 900
-            );
-        }); 
-        it("should handle invalid withdrawals correctly", async () => {
-            await truffleAssert.reverts (
-                wallet.withdraw(500, ticker)
-            );
+            for (let i = 0; i < balances.length; i++) {
+                const {accountId, quantity} = balances[i]
+                await wallet.methods['deposit(uint256,bytes32)'](quantity, ticker, { from: accounts[accountId] });
+
+                let accountBalance = await wallet.balances(accounts[accountId], ticker);
+
+                console.log("deposited to accounts[" + accountId + "]:" + accountBalance);
+                
+                assert(accountBalance.eq(quantity), "Balances do not match");
+            }
         });
-        it("should handle valid withdrawals correctly", async () => {
-            await truffleAssert.passes (
-                wallet.withdraw(100, ticker)
-            );
-            let balanceOfLink = await wallet.balances(accounts[0], ticker);
-            assert.equal(
-                balanceOfLink.toNumber(), 0
-            );
-            let erc20Balance = await link.balanceOf(accounts[0]);
-            assert.equal(
-                erc20Balance.toNumber(), 1000
-            );
+        it("should not allow withdrawals when an account insufficient balance", async () => {
+            for (let i = 0; i < balances.length; i++) {
+                const {accountId, quantity} = balances[i]
+                await truffleAssert.reverts(wallet.methods['withdraw(uint256,bytes32)'](quantity+1, ticker, { from: accounts[accountId] }))
+            }
+        })
+        it("should handle withdrawals correctly", async () => {
+            for (let i = 0; i < balances.length; i++) {
+                const {accountId, quantity} = balances[i]
+                await truffleAssert.passes(wallet.methods['withdraw(uint256,bytes32)'](quantity, ticker, { from: accounts[accountId] }));
+
+                let accountBalance = await wallet.balances(accounts[accountId], ticker);
+
+                console.log("withdrawn from accounts[" + accountId + "]:" + quantity);
+
+                assert(accountBalance.eq(toBN(0)), "Balance MUST be zero");
+            }
         });
     });
 }) 
